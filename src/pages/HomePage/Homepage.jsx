2b2513f7d6a3cf70d5358f9cedd1f2f7bf9f727c
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-01-31 00:07:46 +0200

    try thid

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -13,6 +13,31 @@ const Homepage = () => {
             <LessonsSliderSection />
             <SaysAboutUsSection />
         </div>
+
+
+// import React, { useState, useRef } from "react";
+
+// const App = () => {
+//   const [currentComponent, setCurrentComponent] = useState(0);
+//   const components = [<Component1 />, <Component2 />, <Component3 />];
+//   const containerRef = useRef();
+
+//   const handleScroll = () => {
+//     if (containerRef.current.scrollTop + containerRef.current.clientHeight >= containerRef.current.scrollHeight) {
+//       setCurrentComponent(prevComponent => (prevComponent + 1) % components.length);
+//     }
+//   };
+
+//   return (
+//     <div ref={containerRef} onScroll={handleScroll}>
+//       {components[currentComponent]}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
     );
 }
 
