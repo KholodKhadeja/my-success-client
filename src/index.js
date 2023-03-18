779a8907b7f86c7b342995f30eb3497b4da152b3
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-03-18 22:28:44 +0200

    finishers

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.rtl.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "../node_modules/react-bootstrap/dist/react-bootstrap.js";
+import axios from "axios";
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import store from "./store/index";
 
+axios.defaults.baseURL = `https://my-success.onrender.com/api`;
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  return config;
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <Provider Store={store}>
